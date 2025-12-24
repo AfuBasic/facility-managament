ebf897f5e27510a9720f3da4a9f599bf2b7b50be
Afuwape Babatunde <afutunde@gmail.com>
2025-12-24 13:51:49 +0100

    Created the migrations to create workorder, facitlities, users, clients and workorder assignments

--- database/migrations/2025_12_24_111358_create_client_memberships_table.php
+++ database/migrations/2025_12_24_111358_create_client_memberships_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Client;
+use App\Models\ClientAccount;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -16,8 +16,9 @@ return new class extends Migration
         Schema::create('client_memberships', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class, 'user_id');
-            $table->foreignIdFor(Client::class, 'client_id');
+            $table->foreignIdFor(ClientAccount::class, 'client_account_id');
             $table->enum('status', ['pending','accepted','rejected'])->default('pending');
+            $table->unique(['user_id', 'client_account_id'],'client_membership_user_client_account_unique');
             $table->timestamps();
         });
     }
